8aec72251e7304f27cd0198d9d9f220efda1d5cc
ahtruong1 <andyhtruong01@gmail.com>
2024-03-30 12:17:50 -0400

    Added folder + file structure

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,11 +1,54 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
 import ExampleRoute from "./routes/ExampleRoute.tsx";
+import MedicineRequest from "./routes/MedicineRequest.tsx";
+import Login from "./routes/Login.tsx";
+import Layout from "./routes/Layout.tsx";
+import Home from "./routes/Home.tsx";
+import Services from "./routes/Services.tsx";
+import MedicineConfirmation from "./routes/MedicineConfirmation.tsx";
+import ServiceData from "./routes/ServiceData.tsx";
+import NodeEdgeData from "./routes/NodeEdgeData.tsx";
+
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      errorElement: <div />,
+      element: <Login />,
+    },
+    {
+      path: "/home",
+      element: <Layout />,
+      children: [
+        {
+          path: "/home",
+          element: <Home />,
+        },
+        {
+          path: "/home/services",
+          element: <Services />,
+        },
+        {
+          path: "/home/services/medicine",
+          element: <MedicineRequest />,
+        },
+        {
+          path: "/home/services/medicine/confirmation",
+          element: <MedicineConfirmation />,
+        },
+        {
+          path: "/home/services/all",
+          element: <ServiceData />,
+        },
+        {
+          path: "/home/pathData",
+          element: <NodeEdgeData />,
+        },
+      ],
+    },
+    {
+      path: "/example",
       element: <Root />,
       children: [
         {
--- apps/frontend/src/routes/Layout.tsx
+++ apps/frontend/src/routes/Layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Header from "./Header.tsx";
+import Header from "../components/Header.tsx";
 function Layout() {
   return (
     <div>
