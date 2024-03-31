7567b7026b66d98906d33f9ded3831777b81d448
Lily7jones <lajones@wpi.edu>
2024-03-31 18:10:24 -0400

    working on placement

--- apps/frontend/src/routes/Services.tsx
+++ apps/frontend/src/routes/Services.tsx
@@ -4,23 +4,24 @@ import { Link } from "react-router-dom";
 function Services() {
   return (
     //page together
-    <div>
+    <div className="w-screen h-screen flex-col justify-center">
       {/*page title*/}
-      <div className="text-center text-blue-900 text-[40px] font-bold font-['Roboto'] leading-[42px] tracking-tight">
+      <div className=" text-center text-blue-900 text-[40px] font-bold font-['Roboto'] leading-[42px] tracking-tight">
         Services
       </div>
       <Link to="/MedicineRequest">
         {/* box with service*/}
-        <div className="max-width: 24rem border-2 shadow-md">
-          {" "}
-          {/* container for button*/}
-          <div className="w-36 h-36">
-            {" "}
-            {/*icon*/}
-            <img src={MedicineIcon} alt={"Image of pill and bottle"} />
-          </div>
-          <div className="text-center text-black text-2xl font-normal font-['Roboto']">
-            Medicine Request
+        <div className="w-screen h-screen flex justify-center align-content-center relative">
+          <div className=" w-72 h-72 flex flex-col justify-center align-content-center 24rem border-2 shadow-md relative">
+            {/* container for button*/}
+            <div className="w-36 h-36 flex justify-center align-content-center">
+              {" "}
+              {/*icon*/}
+              <img src={MedicineIcon} alt={"Image of pill and bottle"} />
+            </div>
+            <div className="text-center text-black text-2xl font-normal font-['Roboto']">
+              Medicine Request
+            </div>
           </div>
         </div>
       </Link>
