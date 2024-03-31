0011b7bb6c3bcdafeb0474f874f08fb97fa8857f
Lily7jones <lajones@wpi.edu>
2024-03-31 17:53:11 -0400

    linked button

--- apps/frontend/src/routes/Services.tsx
+++ apps/frontend/src/routes/Services.tsx
@@ -1,19 +1,29 @@
+import MedicineIcon from "../../assets/medicine-icon.png";
+import { Link } from "react-router-dom";
+
 function Services() {
   return (
     //page together
     <div>
-      {/*   page title*/}
+      {/*page title*/}
       <div className="text-center text-blue-900 text-[40px] font-bold font-['Roboto'] leading-[42px] tracking-tight">
         Services
       </div>
-      {/* box with service*/}
-      <div className="max-width: 24rem border-2 shadow-md">
-        {" "}
-        {/*container*/}
-        <div className="text-center text-black text-2xl font-normal font-['Roboto']">
-          Medicine Request
+      <Link to="/MedicineRequest">
+        {/* box with service*/}
+        <div className="max-width: 24rem border-2 shadow-md">
+          {" "}
+          {/* container for button*/}
+          <div className="w-36 h-36">
+            {" "}
+            {/*icon*/}
+            <img src={MedicineIcon} alt={"Image of pill and bottle"} />
+          </div>
+          <div className="text-center text-black text-2xl font-normal font-['Roboto']">
+            Medicine Request
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
