bb1bfe200ae052b340a4e871c614032bf2a0feef
Lily7jones <lajones@wpi.edu>
2024-03-31 17:24:56 -0400

    adding more components

--- apps/frontend/src/routes/Services.tsx
+++ apps/frontend/src/routes/Services.tsx
@@ -1,12 +1,18 @@
-import BlueCircleButton from "../components/BlueCircleButton.tsx";
 function Services() {
   return (
+    //page together
     <div>
+      {/*   page title*/}
       <div className="text-center text-blue-900 text-[40px] font-bold font-['Roboto'] leading-[42px] tracking-tight">
         Services
       </div>
-      <div>
-        <BlueCircleButton />
+      {/* box with service*/}
+      <div className="max-width: 24rem border-2 shadow-md">
+        {" "}
+        {/*container*/}
+        <div className="text-center text-black text-2xl font-normal font-['Roboto']">
+          Medicine Request
+        </div>
       </div>
     </div>
   );
