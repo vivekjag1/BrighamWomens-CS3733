580eb0281847b691936bb69c2a468b2ea54707cb
ahtruong1 <andyhtruong01@gmail.com>
2024-04-01 19:08:41 -0400

    Cleaned up services page

--- apps/frontend/src/routes/Services.tsx
+++ apps/frontend/src/routes/Services.tsx
@@ -1,32 +1,24 @@
-import MedicineIcon from "../../assets/medicine-icon.png";
 import { Link } from "react-router-dom";
-import Paths from "../paths/paths.tsx";
+import medicineIcon from "../../assets/pill-icon.svg";
+import ServiceCard from "../components/ServiceCard.tsx";
+import paths from "../paths/paths.tsx";
 
 function Services() {
   return (
-    //page together
-    <div className="w-screen h-screen flex-col justify-center">
-      {/*page title*/}
-      <div className=" p-9 text-center text-blue-900 text-[40px] font-bold font-['Roboto'] leading-[42px] tracking-tight">
-        Services
-      </div>
-      {/*<Link Paths.MedicineRequest>*/}
-      {/* box with service*/}
-      <div className="w-screen h-screen flex justify-center p-9">
-        <Link to={Paths.MEDICINE_REQUEST}>
-          <div className=" w-72 h-72 flex flex-col justify-center align-content-center 24rem border-2 shadow-md relative hover:bg-zinc-100">
-            {/* container for button*/}
-            <div className="w-36 h-36 absolute bottom-50 left-12">
-              {/*icon*/}
-              <img src={MedicineIcon} alt={"Image of pill and bottle"} />
-            </div>
-            <div className="text-center text-black text-2xl font-normal font-['Roboto'] absolute bottom-10 left-10">
-              Medicine Request
-            </div>
-          </div>
+    <div>
+      <div className="flex flex-col items-center gap-8">
+        <div className="flex flex-col items-center gap-6 mt-8">
+          <h2 className="w-screen text-3xl font-bold text-center">Services</h2>
+          <hr className="w-9/12" />
+        </div>
+        <Link to={paths.MEDICINE_REQUEST}>
+          <ServiceCard
+            imgPath={medicineIcon}
+            alt="capsule and pill icon"
+            label="Medicine Delivery"
+          />
         </Link>
       </div>
-      {/*link here*/}
     </div>
   );
 }
