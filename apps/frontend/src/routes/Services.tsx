49a01830f8f585253d91e95500524d8d3c3385d9
Lily7jones <lajones@wpi.edu>
2024-04-01 09:38:53 -0400

    Formatted Services.tsx

--- apps/frontend/src/routes/Services.tsx
+++ apps/frontend/src/routes/Services.tsx
@@ -6,12 +6,12 @@ function Services() {
     //page together
     <div className="w-screen h-screen flex-col justify-center">
       {/*page title*/}
-      <div className=" text-center text-blue-900 text-[40px] font-bold font-['Roboto'] leading-[42px] tracking-tight">
+      <div className=" p-9 text-center text-blue-900 text-[40px] font-bold font-['Roboto'] leading-[42px] tracking-tight">
         Services
       </div>
       <Link to="/MedicineRequest">
         {/* box with service*/}
-        <div className="w-screen h-screen relative left-50 bottom-50">
+        <div className="w-screen h-screen flex justify-center p-9">
           <div className=" w-72 h-72 flex flex-col justify-center align-content-center 24rem border-2 shadow-md relative hover:bg-zinc-100">
             {/* container for button*/}
             <div className="w-36 h-36 absolute bottom-50 left-12">
