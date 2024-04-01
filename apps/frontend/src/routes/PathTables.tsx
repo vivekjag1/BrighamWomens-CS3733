498101984b7c5efbdfa275b923829675eba8337f
Lily7jones <lajones@wpi.edu>
2024-04-01 10:17:24 -0400

    formatted path tables

--- apps/frontend/src/routes/PathTables.tsx
+++ apps/frontend/src/routes/PathTables.tsx
@@ -14,7 +14,12 @@ function PathTables() {
   return (
     <div>
       <div>
-        <h1 className="text-3xl text-center">Upload File with Node Data</h1>
+        <h1 className="pt-9 text-3xl text-center">
+          Upload File with Node Data
+        </h1>
+        <div className="flex justify-end pr-3">
+          <Button variant="contained">Export Map Data</Button>
+        </div>
         <hr className="m-8" />
         <div className="w-screen h-screen flex flex-col items-center gap-[2vh]">
           <div>
@@ -43,12 +48,11 @@ function PathTables() {
                   />
                 </div>
               </div>
-              <Button variant="contained">Export Map Data</Button>
+              <div className="flex justify-between">
+                <Button variant="contained"> Submit</Button>
+              </div>
             </div>
-            <div>
-              <Button variant="contained"> Submit</Button>
-            </div>
-            <h2 className="text-3xl font-bold mt-[2vh]">Node Table</h2>
+            <h2 className="text-3xl font-bold mt-[2vh] pt-9">Node Table</h2>
             <table className="text-sm text-center text-gray-500 mt-3">
               <thead className="text-xs text-gray-700 uppercase bg-gray-50 drop-shadow-lg">
                 <tr>
@@ -92,8 +96,10 @@ function PathTables() {
               </tbody>
             </table>
           </div>
-          <div className="w-screen h-screen relative left-36">
-            <h2 className="text-3xl font-bold mt-[2vh] left-10">Edge Table</h2>
+          <div className="w-screen h-screen relative left-64">
+            <h2 className="text-3xl font-bold mt-[2vh] left-10 pt-9">
+              Edge Table
+            </h2>
             <table className="text-sm text-center text-gray-500 mt-3">
               <thead className="text-xs text-gray-700 uppercase bg-gray-50 drop-shadow-lg">
                 <tr>
