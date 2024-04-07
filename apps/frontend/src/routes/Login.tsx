165fa6db01f9d30f2d2b53b206465e69f43c5041
Lily7jones <lajones@wpi.edu>
2024-04-07 16:47:42 -0400

    formatting Login.tsx

--- apps/frontend/src/routes/Login.tsx
+++ apps/frontend/src/routes/Login.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import bwhLogoSemiNaked from "../../assets/bwh-logo-semi-naked.svg";
-import bwhExteriorDefault from "../../assets/bwh-exterior-default.png";
+// import bwhExteriorDefault from "../../assets/bwh-exterior-default.png";
 import { TextField, IconButton } from "@mui/material";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import paths from "../paths/paths.tsx";
@@ -22,58 +22,58 @@ function Login() {
   };
 
   return (
-    <div className="w-screen h-screen flex flex-row">
-      <form
-        className="w-[50vw] flex flex-col justify-center items-start px-[10vw] gap-[4vh]"
-        onSubmit={handleLogin}
-      >
-        <img
-          className="w-[500px] h-[60px]"
-          src={bwhLogoSemiNaked}
-          alt="Brigham and Women's Hospital Logo"
-        />
-        <h2 className="font-semibold text-2xl">Sign in</h2>
-        <TextField
-          type="text"
-          label="username"
-          variant="outlined"
-          size="medium"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          sx={{ width: "350px" }}
-        />
-        <TextField
-          type="password"
-          label="password"
-          variant="outlined"
-          size="medium"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          sx={{ width: "350px" }}
-        />
-        {error && <div style={{ color: "red" }}>{error}</div>}
-        <IconButton
-          type="submit"
-          size="small"
-          sx={{
-            width: "65px",
-            height: "65px",
-            backgroundColor: "rgb(0, 156, 166)",
-            borderRadius: "10px",
-            "&:hover": {
-              backgroundColor: "hsl(184, 90%, 33%)",
-            },
-          }}
+    <div className="h-screen w-screen bg-cover bg-[url('../../assets/bwh-exterior-default.png')] flex absolute">
+      {/*<img*/}
+      {/*    className="w-full h-full absolute object-fill"*/}
+      {/*    src={bwhExteriorDefault}*/}
+      {/*    alt="Brigham and Women's Hospital Exterior"*/}
+      {/*/>*/}
+      <div className="h-min w-min justify-center items-center bg-white rounded-2xl absolute">
+        <form
+          className=" flex flex-col justify-center items-center px-[10vw] gap-[4vh]"
+          onSubmit={handleLogin}
         >
-          <ArrowForwardIcon />
-        </IconButton>
-      </form>
-      <div>
-        <img
-          className="w-[60vw] h-full object-cover"
-          src={bwhExteriorDefault}
-          alt="Brigham and Women's Hospital Exterior"
-        />
+          <img
+            className="w-[500px] h-[60px]"
+            src={bwhLogoSemiNaked}
+            alt="Brigham and Women's Hospital Logo"
+          />
+          <h2 className="font-semibold text-2xl">Sign in</h2>
+          <TextField
+            type="text"
+            label="username"
+            variant="outlined"
+            size="medium"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            sx={{ width: "350px" }}
+          />
+          <TextField
+            type="password"
+            label="password"
+            variant="outlined"
+            size="medium"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            sx={{ width: "350px" }}
+          />
+          {error && <div style={{ color: "red" }}>{error}</div>}
+          <IconButton
+            type="submit"
+            size="small"
+            sx={{
+              width: "65px",
+              height: "65px",
+              backgroundColor: "rgb(0, 156, 166)",
+              borderRadius: "10px",
+              "&:hover": {
+                backgroundColor: "hsl(184, 90%, 33%)",
+              },
+            }}
+          >
+            <ArrowForwardIcon />
+          </IconButton>
+        </form>
       </div>
     </div>
   );
