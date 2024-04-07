1488e131965d3f521469c1be08ebcc609fb6fe7e
FrancescoDM6 <112992086+FrancescoDM6@users.noreply.github.com>
2024-04-07 14:18:24 -0400

    added additional service buttons, styled services page

--- apps/frontend/src/routes/Services.tsx
+++ apps/frontend/src/routes/Services.tsx
@@ -8,16 +8,48 @@ function Services() {
     <div>
       <div className="flex flex-col items-center gap-8">
         <div className="flex flex-col items-center gap-6 mt-8">
-          <h2 className="w-screen text-3xl font-bold text-center">Services</h2>
+          <h2 className="w-screen text-4xl font-bold text-center">Services</h2>
           <hr className="w-9/12" />
         </div>
-        <Link to={paths.MEDICINE_REQUEST}>
-          <ServiceCard
-            imgPath={medicineIcon}
-            alt="capsule and pill icon"
-            label="Medicine Delivery"
-          />
-        </Link>
+        <div className="flex justify-center gap-8">
+          <Link to={paths.MEDICINE_REQUEST}>
+            <ServiceCard
+              imgPath={medicineIcon}
+              alt="capsule and pill icon"
+              label="Medicine Delivery"
+            />
+          </Link>
+          <Link to={paths.MEDICAL_DEVICE_DELIVERY}>
+            <ServiceCard
+              imgPath={medicineIcon}
+              alt="capsule and pill icon"
+              label="Medical Device Delivery"
+            />
+          </Link>
+        </div>
+        <div className="flex justify-center gap-8">
+          <Link to={paths.SANITATION_FORM}>
+            <ServiceCard
+              imgPath={medicineIcon}
+              alt="capsule and pill icon"
+              label="Sanitation Request"
+            />
+          </Link>
+          <Link to={paths.ROOM_RESERVATION}>
+            <ServiceCard
+              imgPath={medicineIcon}
+              alt="capsule and pill icon"
+              label="Reserve a Room"
+            />
+          </Link>
+          <Link to={paths.SECURITY_REQUEST}>
+            <ServiceCard
+              imgPath={medicineIcon}
+              alt="capsule and pill icon"
+              label="Security Request"
+            />
+          </Link>
+        </div>
       </div>
     </div>
   );
