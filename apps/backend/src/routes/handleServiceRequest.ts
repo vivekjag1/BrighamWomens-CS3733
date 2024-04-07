e86a4472e6a25c91a7d7b1ba00acf302af553f7c
FatalCodes <134898191+mjrb7@users.noreply.github.com>
2024-04-07 15:44:25 -0400

    Added ability to update the status of service requests with a dropdown in the table

--- apps/backend/src/routes/handleServiceRequest.ts
+++ apps/backend/src/routes/handleServiceRequest.ts
@@ -1,88 +1,68 @@
 import client from "../bin/database-connection.ts";
 import express, { Router, Request, Response } from "express";
-// import { MedicineDeliveryObject } from "common/src/MedicineDelivery.ts";
+import { MedicineDeliveryObject } from "common/src/MedicineDelivery.ts";
+// import { ServiceRequest } from "common/src/ServiceRequest.ts";
 import { ServiceRequest } from "database";
 //import { APIEndpoints } from "common/src/APICommon.ts";
 
 const router: Router = express.Router();
 
-// router.post("/", async function (req: Request, res: Response): Promise<void> {
-//   const request: MedicineDeliveryObject = req.body;
-//
-//   try {
-//     const createdMedicineDelivery = await client.medicineDelivery.create({
-//       data: {
-//         medicineName: request.medicineName,
-//         dosage: request.dosage,
-//         patientName: request.patientName,
-//         serviceRequest: {
-//           create: {
-//             type: "MedicineDelivery",
-//             requestingUsername: request.serviceRequest.requestingUsername,
-//             location: request.serviceRequest.location,
-//             // priority: request.serviceRequest.priority,
-//             // status: request.serviceRequest.status,
-//             // description: request.serviceRequest.description,
-//           },
-//         },
-//       },
-//     });
-//
-//     res.status(200).json({
-//       message: "Medicine delivery created successfully",
-//       data: createdMedicineDelivery,
-//     });
-//   } catch (error) {
-//     console.error("Error creating medicine delivery:", error);
-//     res.status(400).json({ message: "Error creating medicine delivery" });
-//   }
-// });
+router.post("/", async function (req: Request, res: Response): Promise<void> {
+  const request: MedicineDeliveryObject = req.body;
+
+  try {
+    const createdMedicineDelivery = await client.medicineDelivery.create({
+      data: {
+        medicineName: request.medicineName,
+        dosage: request.dosage,
+        patientName: request.patientName,
+        serviceRequest: {
+          create: {
+            type: "MedicineDelivery",
+            requestingUsername: request.serviceRequest.requestingUsername,
+            location: request.serviceRequest.location,
+            description: request.serviceRequest.description,
+            priority: request.serviceRequest.priority,
+            status: request.serviceRequest.status,
+            requestedTime: new Date(),
+            assignedTo: "Bestest Joe",
+          },
+        },
+      },
+    });
+
+    res.status(200).json({
+      message: "Medicine delivery created successfully",
+      data: createdMedicineDelivery,
+    });
+  } catch (error) {
+    console.error("Error creating medicine delivery:", error);
+    res.status(400).json({ message: "Error creating medicine delivery" });
+  }
+});
 
 router.get("/", async function (req: Request, res: Response): Promise<void> {
   const requests: ServiceRequest[] = await client.serviceRequest.findMany();
   res.json(requests);
 });
 
-router.patch(
-  "/:id",
-  async function (req: Request, res: Response): Promise<void> {
-    const { id } = req.params;
-    const { medicineName, dosage, patientName, serviceRequestUpdate } =
-      req.body;
+router.patch("/", async (req, res) => {
+  const { serviceID, status } = req.body;
 
-    try {
-      const updatedMedicineDelivery = await client.$transaction(
-        async (prisma) => {
-          const serviceRequestData = serviceRequestUpdate;
-          const updatedServiceRequest = await prisma.serviceRequest.update({
-            where: { serviceID: Number(id) },
-            data: serviceRequestData,
-          });
+  try {
+    const updatedServiceRequest = await client.serviceRequest.update({
+      where: { serviceID: serviceID },
+      data: { status: status },
+    });
 
-          const updatedMedicine = await prisma.medicineDelivery.update({
-            where: { medicineRequestID: Number(id) },
-            data: {
-              medicineName,
-              dosage,
-              patientName,
-            },
-          });
-
-          return { updatedMedicine, updatedServiceRequest };
-        },
-      );
-
-      res.json({
-        message: "Medicine delivery and service request updated successfully",
-        updatedMedicineDelivery,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(400).json({
-        message: "Error updating medicine delivery and service request",
-      });
-    }
-  },
-);
+    res.json({
+      message: "Service request status updated successfully",
+      updatedServiceRequest,
+    });
+  } catch (error) {
+    console.error("Error updating service request status:", error);
+    res.status(400).json({ message: "Error updating service request status" });
+  }
+});
 
 export default router;
