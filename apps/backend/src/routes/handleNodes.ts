c63b710e62598917185f92eee74a2cfe9bafb6f6
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-04-08 21:46:48 -0400

    Adding code for map editing

--- apps/backend/src/routes/handleNodes.ts
+++ apps/backend/src/routes/handleNodes.ts
@@ -1,11 +1,25 @@
 import client from "../bin/database-connection.ts";
 import express, { Router, Request, Response } from "express";
 import { Node } from "database";
+import { NavigateAttributes } from "common/src/APICommon.ts";
 //import { APIEndpoints } from "common/src/APICommon.ts";
 
 const router: Router = express.Router();
 
 router.get("/", async function (req: Request, res: Response): Promise<void> {
+  if (req.query) {
+    const { [NavigateAttributes.floorKey]: requestedFloor } = req.query;
+
+    const nodesInFloor: Node[] = await client.node.findMany({
+      where: {
+        floor: requestedFloor!.toString(),
+      },
+    });
+
+    res.json(nodesInFloor);
+    return;
+  }
+
   const requests: Node[] = await client.node.findMany();
   res.json(requests);
 });
