7335feaa6640c1a11c9d9035bb6e072d620e1f8d
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-04-09 22:24:01 -0400

    Adding fix for navbar and auth0 stuff

--- apps/frontend/src/archive/SideNavBarV1.tsx
+++ apps/frontend/src/archive/SideNavBarV1.tsx
@@ -39,224 +39,221 @@ function SideNavBarV1() {
     setIsCollapsed(!isCollapsed);
   };
   console.log(isAuthenticated);
-  if (!isAuthenticated) {
-    return <></>;
-  } else {
-    return (
-      <div>
-        <div className=" z-10">
-          <div
-            className={`shadow-lg h-screen bg-secondary flex flex-col space-y-8 transition-width ease-in-out duration-500 ${isCollapsed ? "w-[5rem]" : "w-[11rem]"}`}
-          >
-            <div className=" flex flex-row self-center">
-              <Link to="/home">
-                <div className="shadow-lg rounded-full bg-white mb-10 mt-10 w-60px h-60px flex justify-center items-center">
-                  <img className="h-40px" src={logo} alt="Logo" />
-                </div>
-              </Link>
-              {/*<h2*/}
-              {/*  className="pl-[2rem] font-bold text-white text-md flex items-center max-w-30 whitespace-nowrap overflow-hidden"> {*/}
-              {/*    isCollapsed*/}
-              {/*      ? "hidden"*/}
-              {/*      : "pl-[2rem] font-bold text-white text-md flex items-center max-w-30 whitespace-nowrap overflow-hidden"*/}
-              {/*  }*/}
-              {/*>*/}
-              {/*  {insertLineBreaks("Brigham and Women's Hospital")}*/}
-              {/*</h2>*/}
-            </div>
 
-            <div className="">
-              <div className="ml-[1.5rem] mr-[1.5rem]">
-                <Link to="/home">
-                  <div className="flex flex-row whitespace-pre-wrap">
-                    <MapIcon
-                      sx={{
-                        marginRight: "5px",
-                        marginTop: "-3px",
-                        fontSize: "30px",
-                        color: "white",
-                      }}
-                    />
-                    <h2
-                      className={
-                        isCollapsed
-                          ? "hidden"
-                          : "font-bold text-white text-md whitespace-nowrap"
-                      }
-                    >
-                      Map
-                    </h2>
-                  </div>
-                </Link>
+  return (
+    <div>
+      <div className=" z-10">
+        <div
+          className={`shadow-lg h-screen bg-secondary flex flex-col space-y-8 transition-width ease-in-out duration-500 ${isCollapsed ? "w-[5rem]" : "w-[11rem]"}`}
+        >
+          <div className=" flex flex-row self-center">
+            <Link to="/home">
+              <div className="shadow-lg rounded-full bg-white mb-10 mt-10 w-60px h-60px flex justify-center items-center">
+                <img className="h-40px" src={logo} alt="Logo" />
               </div>
-              <div className="m-[1.5rem]">
-                <Link to="/services">
-                  <div
-                    className="flex flex-row" /*onMouseEnter={toggleHover} onMouseLeave={toggleHover}*/
-                  >
-                    <VolunteerActivismIcon
-                      sx={{
-                        marginRight: "5px",
-                        marginTop: "-3px",
-                        fontSize: "30px",
-                        color: "white",
-                      }}
-                    />
-                    <h2
-                      className={
-                        isCollapsed
-                          ? "hidden"
-                          : "font-bold text-white text-md whitespace-nowrap"
-                      }
-                    >
-                      Services
-                    </h2>
-                  </div>
-                </Link>
-              </div>
-              <div className="m-[1.5rem]">
-                <Link to="/data">
-                  <div className="flex flex-row">
-                    <AddLocationAltIcon
-                      sx={{
-                        marginRight: "5px",
-                        marginTop: "-3px",
-                        fontSize: "30px",
-                        color: "white",
-                      }}
-                    />
-                    <h2
-                      className={
-                        isCollapsed
-                          ? "hidden"
-                          : "font-bold text-white text-md whitespace-nowrap overflow-hidden"
-                      }
-                    >
-                      Map Data
-                    </h2>
-                  </div>
-                </Link>
-              </div>
-              <div className="m-[1.5rem]">
-                <Link to="/services/data">
-                  <div
-                    className="flex flex-row" /*onMouseEnter={toggleHover} onMouseLeave={toggleHover}*/
-                  >
-                    <TocIcon
-                      sx={{
-                        marginRight: "5px",
-                        marginTop: "-3px",
-                        fontSize: "30px",
-                        color: "white",
-                      }}
-                    />
-                    <h2
-                      className={
-                        isCollapsed
-                          ? "hidden"
-                          : "font-bold text-white text-md container whitespace-nowrap overflow-hidden"
-                      }
-                    >
-                      Service Data
-                    </h2>
-                  </div>
-                </Link>
-              </div>
-              <div className="m-[1.5rem]">
-                <Link to="/edit">
-                  <div
-                    className="flex flex-row" /*onMouseEnter={toggleHover} onMouseLeave={toggleHover}*/
-                  >
-                    <EditLocationAltIcon
-                      sx={{
-                        marginRight: "5px",
-                        marginTop: "-3px",
-                        fontSize: "30px",
-                        color: "white",
-                      }}
-                    />
-                    <h2
-                      className={
-                        isCollapsed
-                          ? "hidden"
-                          : "font-bold text-white text-md container whitespace-nowrap overflow-hidden"
-                      }
-                    >
-                      Edit Map
-                    </h2>
-                  </div>
-                </Link>
-              </div>
-              {/*Logout button */}
-              <div className="m-[1.5rem]" onClick={handleLogout}>
-                <Link to="/">
-                  {" "}
-                  {/* enter link to button here to log out*/}
-                  <div
-                    className="flex flex-row" /*onMouseEnter={toggleHover} onMouseLeave={toggleHover}*/
-                  >
-                    <LogoutIcon
-                      sx={{
-                        marginRight: "5px",
-                        marginTop: "-3px",
-                        fontSize: "30px",
-                        color: "white",
-                      }}
-                    />
-                    <h2
-                      className={
-                        isCollapsed
-                          ? "hidden"
-                          : "font-bold text-white text-md container whitespace-nowrap overflow-hidden"
-                      }
-                    >
-                      Logout
-                    </h2>
-                  </div>
-                </Link>
-              </div>
-              <div className="m-[1.5rem] flex">
-                <div
-                  style={{ cursor: "pointer" }}
-                  className="flex flex-row"
-                  onClick={toggleCollapse}
-                >
-                  <div className={isCollapsed ? "" : "hidden"}>
-                    <StartRoundedIcon
-                      sx={{
-                        marginRight: "5px",
-                        marginTop: "-3px",
-                        fontSize: "30px",
-                        color: "white",
-                      }}
-                    />
-                  </div>
-                  <div className={isCollapsed ? "hidden" : ""}>
-                    <KeyboardTabRoundedIcon
-                      sx={{
-                        marginRight: "5px",
-                        marginTop: "-3px",
-                        transform: "scaleX(-1)",
-                        fontSize: "30px",
-                        color: "white",
-                      }}
-                    />
-                  </div>
+            </Link>
+            {/*<h2*/}
+            {/*  className="pl-[2rem] font-bold text-white text-md flex items-center max-w-30 whitespace-nowrap overflow-hidden"> {*/}
+            {/*    isCollapsed*/}
+            {/*      ? "hidden"*/}
+            {/*      : "pl-[2rem] font-bold text-white text-md flex items-center max-w-30 whitespace-nowrap overflow-hidden"*/}
+            {/*  }*/}
+            {/*>*/}
+            {/*  {insertLineBreaks("Brigham and Women's Hospital")}*/}
+            {/*</h2>*/}
+          </div>
+
+          <div className="">
+            <div className="ml-[1.5rem] mr-[1.5rem]">
+              <Link to="/home">
+                <div className="flex flex-row whitespace-pre-wrap">
+                  <MapIcon
+                    sx={{
+                      marginRight: "5px",
+                      marginTop: "-3px",
+                      fontSize: "30px",
+                      color: "white",
+                    }}
+                  />
                   <h2
                     className={
-                      isCollapsed ? "hidden" : "font-bold text-white text-md"
+                      isCollapsed
+                        ? "hidden"
+                        : "font-bold text-white text-md whitespace-nowrap"
                     }
                   >
-                    Collapse
+                    Map
                   </h2>
                 </div>
+              </Link>
+            </div>
+            <div className="m-[1.5rem]">
+              <Link to="/services">
+                <div
+                  className="flex flex-row" /*onMouseEnter={toggleHover} onMouseLeave={toggleHover}*/
+                >
+                  <VolunteerActivismIcon
+                    sx={{
+                      marginRight: "5px",
+                      marginTop: "-3px",
+                      fontSize: "30px",
+                      color: "white",
+                    }}
+                  />
+                  <h2
+                    className={
+                      isCollapsed
+                        ? "hidden"
+                        : "font-bold text-white text-md whitespace-nowrap"
+                    }
+                  >
+                    Services
+                  </h2>
+                </div>
+              </Link>
+            </div>
+            <div className="m-[1.5rem]">
+              <Link to="/data">
+                <div className="flex flex-row">
+                  <AddLocationAltIcon
+                    sx={{
+                      marginRight: "5px",
+                      marginTop: "-3px",
+                      fontSize: "30px",
+                      color: "white",
+                    }}
+                  />
+                  <h2
+                    className={
+                      isCollapsed
+                        ? "hidden"
+                        : "font-bold text-white text-md whitespace-nowrap overflow-hidden"
+                    }
+                  >
+                    Map Data
+                  </h2>
+                </div>
+              </Link>
+            </div>
+            <div className="m-[1.5rem]">
+              <Link to="/services/data">
+                <div
+                  className="flex flex-row" /*onMouseEnter={toggleHover} onMouseLeave={toggleHover}*/
+                >
+                  <TocIcon
+                    sx={{
+                      marginRight: "5px",
+                      marginTop: "-3px",
+                      fontSize: "30px",
+                      color: "white",
+                    }}
+                  />
+                  <h2
+                    className={
+                      isCollapsed
+                        ? "hidden"
+                        : "font-bold text-white text-md container whitespace-nowrap overflow-hidden"
+                    }
+                  >
+                    Service Data
+                  </h2>
+                </div>
+              </Link>
+            </div>
+            <div className="m-[1.5rem]">
+              <Link to="/edit">
+                <div
+                  className="flex flex-row" /*onMouseEnter={toggleHover} onMouseLeave={toggleHover}*/
+                >
+                  <EditLocationAltIcon
+                    sx={{
+                      marginRight: "5px",
+                      marginTop: "-3px",
+                      fontSize: "30px",
+                      color: "white",
+                    }}
+                  />
+                  <h2
+                    className={
+                      isCollapsed
+                        ? "hidden"
+                        : "font-bold text-white text-md container whitespace-nowrap overflow-hidden"
+                    }
+                  >
+                    Edit Map
+                  </h2>
+                </div>
+              </Link>
+            </div>
+            {/*Logout button */}
+            <div className="m-[1.5rem]" onClick={handleLogout}>
+              <Link to="/">
+                {" "}
+                {/* enter link to button here to log out*/}
+                <div
+                  className="flex flex-row" /*onMouseEnter={toggleHover} onMouseLeave={toggleHover}*/
+                >
+                  <LogoutIcon
+                    sx={{
+                      marginRight: "5px",
+                      marginTop: "-3px",
+                      fontSize: "30px",
+                      color: "white",
+                    }}
+                  />
+                  <h2
+                    className={
+                      isCollapsed
+                        ? "hidden"
+                        : "font-bold text-white text-md container whitespace-nowrap overflow-hidden"
+                    }
+                  >
+                    Logout
+                  </h2>
+                </div>
+              </Link>
+            </div>
+            <div className="m-[1.5rem] flex">
+              <div
+                style={{ cursor: "pointer" }}
+                className="flex flex-row"
+                onClick={toggleCollapse}
+              >
+                <div className={isCollapsed ? "" : "hidden"}>
+                  <StartRoundedIcon
+                    sx={{
+                      marginRight: "5px",
+                      marginTop: "-3px",
+                      fontSize: "30px",
+                      color: "white",
+                    }}
+                  />
+                </div>
+                <div className={isCollapsed ? "hidden" : ""}>
+                  <KeyboardTabRoundedIcon
+                    sx={{
+                      marginRight: "5px",
+                      marginTop: "-3px",
+                      transform: "scaleX(-1)",
+                      fontSize: "30px",
+                      color: "white",
+                    }}
+                  />
+                </div>
+                <h2
+                  className={
+                    isCollapsed ? "hidden" : "font-bold text-white text-md"
+                  }
+                >
+                  Collapse
+                </h2>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default SideNavBarV1;
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,9 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      useRefreshTokens
+      cacheLocation="localstorage"
+      domain="dev-7eoh0ojk0tkfhypo.us.auth0.com"
+      clientId="U8XpuA4s1L8lmd1avUIOupo1494YlppB"
+      authorizationParams={{
+        redirect_uri: "http://localhost:3000/home",
+        //scope: "openid profile email offline_access",
+      }}
+      // onRedirectCallback={(appState) => {
+      //   navigate(appState?.returnTo || window.location.pathname);
+      // }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
 );
