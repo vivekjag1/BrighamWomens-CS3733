6152e555311e57844ecc92614ca67ff7b45c67b7
ahtruong1 <andyhtruong01@gmail.com>
2024-04-09 20:19:07 -0400

    redesigned login page

--- apps/frontend/src/routes/Login.tsx
+++ apps/frontend/src/routes/Login.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import bwhLogoSemiNaked from "../../assets/bwh-logo-semi-naked.svg";
+import "./Login.css";
+/*import { useAuth0 } from "@auth0/auth0-react";*/
 import { useNavigate } from "react-router-dom";
 
 function Login() {
   const navigate = useNavigate();
-  const { loginWithRedirect } = useAuth0();
-  async function handleLogin(e: React.FormEvent<HTMLFormElement>) {
+  /*const { loginWithRedirect } = useAuth0();*/
+  /*  async function handleLogin(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     await loginWithRedirect({
       appState: {
@@ -14,42 +14,31 @@ function Login() {
       },
     });
     console.log("hello world");
-  }
+  }*/
   function handlePathfind() {
     navigate("/home");
   }
 
   return (
-    <div className="h-screen w-screen bg-cover bg-[url('../../assets/bwh-exterior-default.png')]  relative">
-      <div className="h-screen w-screen relative flex flex-col justify-center items-center">
-        <form
-          className="flex flex-col items-start justify-start align-middle gap-2 p-8 bg-white rounded-2xl dropshadow-2xlg hover:ring-white hover:ring-8 absolute"
-          onSubmit={handleLogin}
-        >
-          <button type="submit" className="flex flex-col items-center gap-2 ">
-            <img
-              className="w-[500px] h-[60px]"
-              src={bwhLogoSemiNaked}
-              alt="Brigham and Women's Hospital Logo"
-            />
-            <p className="font-bold text-2xl text-blue-900">
-              Are you a staff member? Click to Login
-            </p>
-          </button>
-          <div className="flex flex-col items-start justify-start align-middle gap-2">
-            <button
-              type="submit"
-              className="flex flex-col justify-center"
-              onClick={handlePathfind}
-            >
-              <p className="font-bold text-2xl text-blue-900">
-                Click here to navigate through the hospital!
-              </p>
-            </button>
-          </div>
-        </form>
+    <div>
+      <div
+        onClick={handlePathfind}
+        className="landing-page w-screen h-screen bg-no-repeat bg-cover bg-center"
+      >
+        <div className="opening-remarks w-screen h-screen flex flex-col justify-center items-center gap-12">
+          <h2 className="text-7xl font-semibold text-white">
+            Welcome to Brigham and Women's Hospital
+          </h2>
+          <p className="font-normal text-5xl text-white">
+            Navigation made simple
+          </p>
+          <p className="font-light text-3xl text-white">
+            Touch the screen to begin.
+          </p>
+        </div>
       </div>
     </div>
   );
 }
+
 export default Login;
