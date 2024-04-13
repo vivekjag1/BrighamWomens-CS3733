49da60922634fa1a5cfafed5ebcfbd7855345c8e
ColinMasucci <colined14@gmail.com>
2024-04-13 15:22:13 -0400

    still not scaling

--- apps/frontend/src/components/ZoomControls.tsx
+++ apps/frontend/src/components/ZoomControls.tsx
@@ -5,18 +5,20 @@ function ZoomControls() {
   const styles = {
     color: "#FFFFFF",
     backgroundColor: "#012D5A",
-    width: "3vw",
+    width: "1vw",
     height: "5vh",
+    minWidth: "1vw",
     "&:hover": {
       backgroundColor: "rgba(0,0,0,0.15)",
+      minWidth: "0vw",
     },
   } as const;
 
   const { zoomIn, zoomOut, resetTransform } = useControls();
   return (
-    <div className="absolute bottom-[30%] right-[2%] z-10">
+    <div className="fixed bottom-[30%] right-[2%] z-10">
       <ButtonGroup
-        sx={{ backgroundColor: "#013B96", height: "100%" }}
+        sx={{ backgroundColor: "#013B96", height: "100%", minWidth: "0px" }}
         orientation="vertical"
         variant="contained"
         aria-label="Basic button group"
