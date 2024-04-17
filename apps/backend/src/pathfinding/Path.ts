82b5110bfd1d02bc1558d6ae02eba53e070021e9
griffin <gbrown5813@gmail.com>
2024-04-16 21:26:13 -0400

    Fixed map image randomly flying around (fingers crossed)

--- apps/backend/src/pathfinding/Path.ts
+++ apps/backend/src/pathfinding/Path.ts
@@ -1,4 +1,10 @@
+import { Graph } from "./Graph.ts";
+
 export interface Path {
+  startNodeID: string;
+  endNodeID: string;
+  parentGraph: Graph;
+  path: string[];
   getPath(): string[];
   createPath(startNodeID: string, endNodeID: string): string[];
 }
