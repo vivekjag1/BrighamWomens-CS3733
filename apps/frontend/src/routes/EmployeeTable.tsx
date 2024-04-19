a6116214ec8d8d3875c5631e2628fc57dd7feedd
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-04-19 19:56:15 -0400

    Adding role checks for map data page

--- apps/frontend/src/routes/EmployeeTable.tsx
+++ apps/frontend/src/routes/EmployeeTable.tsx
@@ -232,7 +232,7 @@ const EmployeeTable = () => {
                       </div>
                     </div>
                   </th>
-                  <td className="px-6 py-4">Edger</td>
+                  <td className="px-6 py-4">Auth0 God</td>
                   <td className="px-6 py-4">
                     <div className="flex items-center">
                       <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div>
