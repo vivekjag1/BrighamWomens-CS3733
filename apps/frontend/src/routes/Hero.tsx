6c5d25d2edf9cbda62482b10cb7bb8e5fbfaa4e3
dgorbunov <38931252+dgorbunov@users.noreply.github.com>
2024-04-19 11:34:03 -0400

    new hero disclaimer

--- apps/frontend/src/routes/Hero.tsx
+++ apps/frontend/src/routes/Hero.tsx
@@ -16,18 +16,18 @@ function Hero() {
         className="landing-page w-screen h-screen bg-no-repeat bg-cover bg-center"
       >
         <div className="opening-remarks w-screen h-screen flex flex-col justify-center items-center gap-8">
-          <label className="absolute top-[2vh] font-light text-[1vw] text-white">
-            *<span className="underline">Disclaimer</span>: This website is a
-            term project exercise for WPI CS 3733 Software Engineering (Prof.
+          <label className="bg-white absolute top-[2vh] font-light text-[1vw] rounded-xl p-3 text-secondary">
+            <span className="underline font-italic">Disclaimer</span>: This
+            website is a project for WPI CS 3733 Software Engineering (Prof.
             Wong) and is to not be confused with the actual Brigham & Women's
             Hospital Website.
           </label>
           <label className="text-[3vw] font-semibold text-white">
             Welcome to Brigham and Women's Hospital
           </label>
-          <label className="font-normal text-[2vw] text-white">
-            Navigation made simple
-          </label>
+          {/*<label className="font-normal text-[2vw] text-white">*/}
+          {/*  Navigation made simple*/}
+          {/*</label>*/}
           <label className="font-light text-[2vw] text-white animate-pulse">
             Touch the screen to begin.
           </label>
