42000968560a80daadae62ff3c64dbb6ac04db5e
Andy Truong <ahtruong@wpi.edu>
2024-04-19 02:26:57 -0400

    adjusted delay of disclaimer banner

--- apps/frontend/src/components/Banner/Banner.tsx
+++ apps/frontend/src/components/Banner/Banner.tsx
@@ -11,7 +11,7 @@ function Banner(props: BannerProps) {
     <motion.div
       className="w-[80vw] h-[5.5vh] flex justify-center items-center gap-8 border-2 border-gray-200 rounded-md shadow-md bg-gray-100"
       initial={{ y: "-10vh" }}
-      animate={{ y: 0, transition: { delay: 1.5, duration: 1 } }}
+      animate={{ y: 0, transition: { delay: 3, duration: 1 } }}
       exit={{ y: "-10vh", transition: { duration: 1 } }}
     >
       {props.children}
