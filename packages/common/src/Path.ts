f4e881c1db1be2aead66d8cc1ca47c07f6ca38b6
dgorbunov <38931252+dgorbunov@users.noreply.github.com>
2024-04-24 00:20:06 -0400

    add textual path type defs

--- packages/common/src/Path.ts
+++ packages/common/src/Path.ts
@@ -1,6 +1,54 @@
-export type PathNodesObject = {
+export type PathType = {
   startNode: string;
   endNode: string;
 };
 
 export type PathAlgorithm = "A-Star" | "BFS" | "DFS" | "Dijkstra";
+
+export enum DirectionType {
+  Start,
+  End,
+  Straight,
+  Left,
+  SlightLeft,
+  HairpinLeft,
+  Right,
+  SlightRight,
+  HairpinRight,
+  Elevator,
+  Stairs,
+}
+
+export const DirectionMessages = new Map<DirectionType, string>([
+  [DirectionType.Start, "Start at"],
+  [DirectionType.End, "Arrived at"],
+  [DirectionType.Straight, "Go straight for"],
+  [DirectionType.Left, "Turn left"],
+  [DirectionType.SlightLeft, "Turn slight left"],
+  [DirectionType.HairpinLeft, "Turn hairpin left"],
+  [DirectionType.Right, "Turn right"],
+  [DirectionType.SlightRight, "Turn slight right"],
+  [DirectionType.HairpinRight, "Turn hairpin right"],
+  [DirectionType.Elevator, "Take the elevator to floor"],
+  [DirectionType.Stairs, "Take the stairs to floor"],
+]);
+
+export type DirectionMessage = {
+  type: DirectionType;
+  msg: string;
+};
+
+enum StatUnit {
+  Mins,
+  Distance,
+}
+
+export type TripStat = {
+  stat: string;
+  unit: StatUnit;
+};
+
+export type Directions = {
+  directions: DirectionMessage[];
+  floor: string;
+};
