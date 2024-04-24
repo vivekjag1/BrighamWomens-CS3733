4e407bdea86cd77cc4217183e16096e723bfb9b2
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-04-24 10:26:27 -0400

    mapedit fixes

--- apps/backend/src/routes/map/createNode.ts
+++ apps/backend/src/routes/map/createNode.ts
@@ -3,11 +3,12 @@ const router: Router = express.Router();
 import { PrismaClient } from "database";
 import { Node } from "database";
 const prisma = new PrismaClient();
-
+console.log("inside API");
 router.post("/", async (req, res) => {
-  const recNode: Node = req.body;
+  const recNode: Node = req.body.nodes;
+  console.log("rec node", recNode);
   try {
-    console.log(recNode);
+    console.log("the node recieved is", recNode);
     const createdNode = await prisma.node.createMany({ data: recNode });
     res.status(200).json({ nodes: createdNode });
   } catch (error) {
