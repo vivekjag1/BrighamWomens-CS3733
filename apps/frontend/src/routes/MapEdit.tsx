e4d966c3c3ef5a73e11bc9896f697da7796766b8
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-04-26 15:32:28 -0400

    edit for mapedit updating nodes not working

--- apps/frontend/src/routes/MapEdit.tsx
+++ apps/frontend/src/routes/MapEdit.tsx
@@ -7,7 +7,6 @@ import MapFloorSelect from "../components/MapFloorSelect.tsx";
 import MapEditCard from "../components/MapEditCard.tsx";
 import MapData from "./MapData.tsx";
 import { useAuth0 } from "@auth0/auth0-react";
-// import { useToast } from "../components/useToast.tsx";
 import MapEditToolBar from "../components/MapEditToolBar.tsx";
 import { MakeProtectedPostRequest } from "../MakeProtectedPostRequest.ts";
 import { MakeProtectedGetRequest } from "../MakeProtectedGetRequest.ts";
@@ -95,7 +94,6 @@ function MapEdit() {
   const { getAccessTokenSilently } = useAuth0();
   useEffect(() => {
     const fetchData = async () => {
-      // const token = await getAccessTokenSilently();
       let activeFloorString;
       switch (activeFloor) {
         case -1:
@@ -170,6 +168,7 @@ function MapEdit() {
   };
 
   function updateNode(node: Node) {
+    console.log("inside updateNode");
     const tempNodes = new Map(nodes);
     const tempUpdatedNodes = new Map(updatedNodes);
     if (selectedNodeID) {
@@ -235,11 +234,6 @@ function MapEdit() {
     const addedRepairedEdges = tempRepairedEdges.concat(addedEdges);
     setEdges(updatedTempEdges);
     setAddedEdges(addedRepairedEdges);
-
-    // const sendToDb = {
-    //   nodeID: selectedNodeID,
-    // };
-    // await axios.post(APIEndpoints.deleteNode, sendToDb);
   }
 
   function handleNodeClick(nodeID: string) {
@@ -254,8 +248,6 @@ function MapEdit() {
       if (!startEdgeNodeID) {
         setStartEdgeNodeID(nodeID);
       } else {
-        //const endNodeID = nodeID;
-        //Create an edge logic goes here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         setStartEdgeNodeID(undefined);
       }
     } else {
@@ -271,11 +263,6 @@ function MapEdit() {
       setNodeSaved(false);
     }
   }
-
-  // function handleAddNodeButtonClicked() {
-  //   setAddingNode(!addingNode);
-  //   setAddingEdge(false);
-  // }
 
   function handleSelectNodeSelected() {
     setSelectedAction(Action.SelectNode);
@@ -308,9 +295,6 @@ function MapEdit() {
 
   async function handleSaveAll() {
     const token = await getAccessTokenSilently();
-    // const sendNodes = {
-    //   nodes: Array.from(addedNodes.values()),
-    // };
     await MakeProtectedPostRequest(
       APIEndpoints.createNode,
       Array.from(addedNodes.values()),
@@ -354,16 +338,20 @@ function MapEdit() {
   }
 
   async function handleSave() {
+    console.log("inside handle save!");
+    console.log("nodes that have been updated", updatedNodes);
     const token = await getAccessTokenSilently();
-    // if (nodes.get(selectedNodeID!)!.shortName === "") {
-    //   showToast("Please fill in a short name!", "error");
-    //   return;
-    // }
 
     const node = nodes.get(selectedNodeID!);
 
     if (node!.nodeID.substring(0, 8) != "userNode") {
-      //await MakeProtectedPatchRequest(APIEndpoints.updateNodes, node!, token);
+      const sendToBackend: Node[] = [];
+      sendToBackend.push(node!);
+      console.log(node);
+      const send = {
+        nodes: sendToBackend,
+      };
+      await MakeProtectedPatchRequest(APIEndpoints.updateNodes, send, token);
     } else {
       //cut first 8 characters
 
