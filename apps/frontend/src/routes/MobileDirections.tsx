787e9f31327c02f27e038b408e3addce147285ed
dgorbunov <38931252+dgorbunov@users.noreply.github.com>
2024-04-26 19:54:02 -0400

    fix navigation api call in mobile directions page

--- apps/frontend/src/routes/MobileDirections.tsx
+++ apps/frontend/src/routes/MobileDirections.tsx
@@ -5,17 +5,18 @@ import { useEffect } from "react";
 function MobileDirections() {
   async function getPath() {
     // Get query params from URL
-    const queryParams: string[][] = [];
+    const queryParams: Record<string, string> = {};
+
     const queryString = window.location.search.substring(1); // search after ?
     const pairs = queryString.split("&"); // split by &
 
     for (let i = 0; i < pairs.length; i++) {
       const pair = pairs[i].split("="); // split into key and value
-      queryParams[i] = [pair[0], pair[1]];
+      queryParams[pair[0]] = pair[1];
     }
 
-    const url = new URL(APIEndpoints.mobileDirections, window.location.origin); // url to send request to
-    url.search = queryParams.toString();
+    const url = new URL(APIEndpoints.navigationRequest, window.location.origin); // url to send request to
+    url.search = new URLSearchParams(queryParams).toString();
 
     // Send request to backend
     await axios
