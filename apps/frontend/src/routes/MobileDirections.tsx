bff1b69373c4c81c458c0a511ea0c0c81e8598ce
dgorbunov <38931252+dgorbunov@users.noreply.github.com>
2024-04-26 20:08:28 -0400

    add useStates and parsing for mobile directions

--- apps/frontend/src/routes/MobileDirections.tsx
+++ apps/frontend/src/routes/MobileDirections.tsx
@@ -1,36 +1,46 @@
 import axios from "axios";
 import { APIEndpoints } from "common/src/APICommon.ts";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Directions, TripStat } from "common/src/Path.ts";
 
 function MobileDirections() {
-  async function getPath() {
-    // Get query params from URL
-    const queryParams: Record<string, string> = {};
-
-    const queryString = window.location.search.substring(1); // search after ?
-    const pairs = queryString.split("&"); // split by &
-
-    for (let i = 0; i < pairs.length; i++) {
-      const pair = pairs[i].split("="); // split into key and value
-      queryParams[pair[0]] = pair[1];
-    }
-
-    const url = new URL(APIEndpoints.navigationRequest, window.location.origin); // url to send request to
-    url.search = new URLSearchParams(queryParams).toString();
-
-    // Send request to backend
-    await axios
-      .get(url.toString())
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch(console.error);
-  }
+  const [directions, setDirections] = useState<Directions[]>([]);
+  const [tripStats, setTripStats] = useState<TripStat[]>([]);
 
   // Runs on page load
   useEffect(() => {
+    async function getPath() {
+      // Get query params from URL
+      const queryParams: Record<string, string> = {};
+
+      const queryString = window.location.search.substring(1); // search after ?
+      const pairs = queryString.split("&"); // split by &
+
+      for (let i = 0; i < pairs.length; i++) {
+        const pair = pairs[i].split("="); // split into key and value
+        queryParams[pair[0]] = pair[1];
+      }
+
+      const url = new URL(
+        APIEndpoints.navigationRequest,
+        window.location.origin,
+      ); // url to send request to
+      url.search = new URLSearchParams(queryParams).toString();
+
+      // Send request to backend
+      await axios
+        .get(url.toString())
+        .then((response) => {
+          setDirections(response.data.directions);
+          setTripStats(response.data.tripStats);
+          console.log(directions);
+          console.log(tripStats);
+        })
+        .catch(console.error);
+    }
+
     getPath();
-  }, []);
+  });
 
   return <div></div>;
 }
