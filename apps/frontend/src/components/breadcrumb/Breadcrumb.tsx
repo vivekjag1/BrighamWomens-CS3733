5d7aa1597af7f2360593cac4bb6c0d47d2942f68
Andy Truong <ahtruong@wpi.edu>
2024-04-28 10:16:23 -0400

    fixed bug, where acordion would expand into a new page with dfs

--- apps/frontend/src/components/breadcrumb/Breadcrumb.tsx
+++ apps/frontend/src/components/breadcrumb/Breadcrumb.tsx
@@ -6,14 +6,14 @@ interface breadcrumbProps {
 function Breadcrumb(props: breadcrumbProps) {
   return (
     <motion.div
-      className="flex justify-center items-center h-[2.5vw] border border-gray-200 rounded-lg bg-gray-50 p-6 drop-shadow-lg"
+      className="min-h-[2.5vw] max-h-[5vw] p-5 flex items-center border border-gray-200 rounded-lg bg-gray-50  drop-shadow-lg"
       initial={{ scaleX: 0 }}
       animate={{ scaleX: 1 }}
       exit={{ scaleX: 0 }}
       transition={{ duration: 1, ease: "easeInOut", delay: 0.5 }}
     >
       <motion.div
-        className="flex justify-center items-center"
+        className="flex max-w-[37vw] flex-wrap gap-2"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1, transition: { delay: 1.5 } }}
         exit={{ opacity: 0, transition: { duration: 1 } }}
