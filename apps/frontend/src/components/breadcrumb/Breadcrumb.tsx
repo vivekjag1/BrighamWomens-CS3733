894566af85ecd00f68141caa439119692c1aae47
Andy Truong <ahtruong@wpi.edu>
2024-04-28 10:22:05 -0400

    adjusted padding, so accordion isnt a thicky anymore

--- apps/frontend/src/components/breadcrumb/Breadcrumb.tsx
+++ apps/frontend/src/components/breadcrumb/Breadcrumb.tsx
@@ -6,14 +6,14 @@ interface breadcrumbProps {
 function Breadcrumb(props: breadcrumbProps) {
   return (
     <motion.div
-      className="min-h-[2.5vw] max-h-[5vw] p-5 flex items-center border border-gray-200 rounded-lg bg-gray-50  drop-shadow-lg"
+      className="min-h-[2.5vw] max-h-[5vw] flex items-center border border-gray-200 rounded-lg bg-gray-50  drop-shadow-lg"
       initial={{ scaleX: 0 }}
       animate={{ scaleX: 1 }}
       exit={{ scaleX: 0 }}
       transition={{ duration: 1, ease: "easeInOut", delay: 0.5 }}
     >
       <motion.div
-        className="flex max-w-[37vw] flex-wrap gap-2"
+        className="flex max-w-[31vw] flex-wrap gap-2 p-3"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1, transition: { delay: 1.5 } }}
         exit={{ opacity: 0, transition: { duration: 1 } }}
