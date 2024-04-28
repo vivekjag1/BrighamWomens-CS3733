ae4775b996db1c1ccbd591c9bccccd7350b3d78e
ahtruong1 <andyhtruong01@gmail.com>
2024-04-28 13:07:08 -0400

    start and end markers on 3d map now match markers on 2d map

--- apps/frontend/src/components/map/StartEndMarker.tsx
+++ apps/frontend/src/components/map/StartEndMarker.tsx
@@ -20,10 +20,9 @@ function StartEndMarker(props: startEndMarkerProps) {
       >
         <motion.path
           fill={props.color}
-          stroke="black"
           d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7m0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5"
         ></motion.path>
-        <motion.circle cx={12} cy={9} fill="black" r={2.25}></motion.circle>
+        <motion.circle cx={12} cy={9} fill="white" r={2.65}></motion.circle>
       </motion.g>
     </svg>
   );
