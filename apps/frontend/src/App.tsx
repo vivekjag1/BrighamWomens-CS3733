e7c92b403b2ea92ea58884a5b615410663c9535d
Mohamed Adem <mdjadid@wpi.edu>
2024-05-01 00:28:42 -0400

    functional custom BoundaryError page.

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,40 +1,70 @@
+import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import paths from "./common/paths.tsx";
-import routes from "./common/routes.tsx";
-import Layout from "./components/Layout.tsx";
-import Hero from "./routes/hero/Hero.tsx";
-import MobileDirections from "./routes/MobileDirections.tsx";
-import AboutUs from "./routes/AboutUs.tsx";
-import Credit from "./routes/Credit.tsx";
+import paths from "./common/paths";
+import routes from "./common/routes";
+import Layout from "./components/Layout";
+import Hero from "./routes/hero/Hero";
+import MobileDirections from "./routes/MobileDirections";
+import AboutUs from "./routes/AboutUs";
+import Credit from "./routes/Credit";
 import NotFound from "./routes/NotFound";
+import ErrorFallBack from "./ErrorFallback.tsx";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: paths.HERO,
-      element: <Hero />,
-    },
-    {
-      path: "*",
-      element: <NotFound />,
+      element: (
+        <ErrorBoundary FallbackComponent={ErrorFallBack}>
+          <Hero />
+        </ErrorBoundary>
+      ),
     },
 
     {
+      path: "*",
+      element: (
+        <ErrorBoundary FallbackComponent={ErrorFallBack}>
+          <NotFound />
+        </ErrorBoundary>
+      ),
+    },
+    {
       path: paths.HOME,
       element: <Layout />,
-      children: routes,
+      children: routes.map((route) => ({
+        ...route,
+        element: (
+          <ErrorBoundary FallbackComponent={ErrorFallBack}>
+            {route.element}
+          </ErrorBoundary>
+        ),
+      })),
     },
     {
       path: paths.MOBILE_DIRECTIONS,
-      element: <MobileDirections />,
+      element: (
+        <ErrorBoundary FallbackComponent={ErrorFallBack}>
+          <MobileDirections />
+        </ErrorBoundary>
+      ),
     },
     {
       path: paths.ABOUT,
-      element: <AboutUs />,
+      element: (
+        <ErrorBoundary FallbackComponent={ErrorFallBack}>
+          <AboutUs />
+        </ErrorBoundary>
+      ),
     },
     {
       path: paths.CREDITS,
-      element: <Credit />,
+      element: (
+        <ErrorBoundary FallbackComponent={ErrorFallBack}>
+          <Credit />
+        </ErrorBoundary>
+      ),
     },
   ]);
 
